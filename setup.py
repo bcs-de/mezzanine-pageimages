cf1708aa91a689414b3ae19745a08866b0fcfea4
Arnold Krille <a.krille@b-c-s.de>
2013-10-24 14:44:07 +0200

    initial creation of the app

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from distutils.core import setup
+
+setup(
+    name="mezzanine-pageimages",
+    version="0.1.0",
+    description="",
+    author="Arnold Krille for bcs kommunikationsloesungen",
+    author_email="a.krille@b-c-s.de",
+    url="http://github.com/bcs-de/mezzanine-pageimages",
+    license=open('LICENSE', 'r').read(),
+    packages=['pageimages'],
+    #package_data={'workout': ['templates/workout/*']},
+    #install_requires=['fitparse==0.0.1-dev'],
+    #dependency_links=['git+https://github.com/kampfschlaefer/python-fitparse.git@ng#egg=fitparse-0.0.1-dev'],
+)
