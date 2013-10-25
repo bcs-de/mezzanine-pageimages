fd53271c0e95bf2b2dced62ae822e51d2421a43b
Arnold Krille <a.krille@b-c-s.de>
2013-10-25 11:50:45 +0200

    Start writing tests

--- pageimages/tests.py
+++ pageimages/tests.py
@@ -15,19 +15,63 @@
 #   limitations under the License.
 #
 
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from mezzanine.pages.models import Page
+
+from .models import DefaultImage, PageImage
+from .templatetags.pageimage_tags import get_image_for_page
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestGetImageForPage(TestCase):
+    def setUp(self):
+        self.p = Page(title='test')
+        self.p.save()
+
+    def setUpDefaultImage(self):
+        di = DefaultImage(file='default.jpg', type='BACKGROUND')
+        di.save()
+
+    def setUpPageImage(self):
+        pi = PageImage(page=self.p, type='BACKGROUND', file='test.jpg')
+        pi.save()
+
+    def test_get_empty(self):
+        self.assertEqual(
+            '',
+            get_image_for_page(self.p, 'BACKGROUND')
+        )
+
+    def test_get_default_image(self):
+        self.setUpDefaultImage()
+
+        self.assertEqual(
+            '/static/media/default.jpg',
+            get_image_for_page(self.p, 'BACKGROUND')
+        )
+
+    def test_get_specific_image(self):
+        self.setUpDefaultImage()
+        self.setUpPageImage()
+
+        self.assertEqual(
+            '/static/media/test.jpg',
+            get_image_for_page(self.p, 'BACKGROUND')
+        )
+
+    def test_get_child_images(self):
+        self.setUpDefaultImage()
+        self.setUpPageImage()
+
+        p1 = Page.objects.create(title='child1', parent=self.p)
+        p2 = Page.objects.create(title='child2', parent=self.p)
+
+        PageImage.objects.create(page=p2, type='BACKGROUND', file='child2.jpg')
+
+        self.assertEqual(
+            '/static/media/test.jpg',
+            get_image_for_page(p1, 'BACKGROUND')
+        )
+        self.assertEqual(
+            '/static/media/child2.jpg',
+            get_image_for_page(p2, 'BACKGROUND')
+        )
