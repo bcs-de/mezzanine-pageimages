7ff6fd49c2ac0254bd711e0aa6259c8e253cf0af
Arnold Krille <a.krille@b-c-s.de>
2013-10-25 16:42:46 +0200

    Fix packaging and make it version 0.2.1

--- setup.py
+++ setup.py
@@ -15,16 +15,28 @@
 #   limitations under the License.
 #
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="mezzanine-pageimages",
-    version="0.2.0",
-    description="",
+    version="0.2.1",
+    description="Define (background-/banner-)images per page in mezzanine.",
     author="Arnold Krille for bcs kommunikationsloesungen",
     author_email="a.krille@b-c-s.de",
     url="http://github.com/bcs-de/mezzanine-pageimages",
-    license=open('LICENSE', 'r').read(),
+    license=''
+    'Licensed under the Apache License, Version 2.0 (the "License");'
+    'you may not use this file except in compliance with the License.'
+    'You may obtain a copy of the License at'
+    ''
+    '  http://www.apache.org/licenses/LICENSE-2.0'
+    ''
+    'Unless required by applicable law or agreed to in writing, software'
+    'distributed under the License is distributed on an "AS IS" BASIS,'
+    'WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.'
+    'See the License for the specific language governing permissions and'
+    'limitations under the License.',
+
     packages=['pageimages', 'pageimages.templatetags', 'pageimages.migrations'],
     install_requires=['mezzanine>=1.4.0'],
 )
